42c9352e34ce6c17e0ca68143edeb7743b697950
Boonchuay Raksom <prototype.2530@gmail.com>
2020-02-20 17:48:40 +0700

    Initial Project Add

---
+++ src/test/java/com/keereesoft/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keereesoft.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
